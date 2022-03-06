4830fb37cf7d9427f85f86bb1f197b4e57ce76a3
Marcos Minond <marcos.minond@mx.com>
2022-03-06 11:43:36 -0700

    Renaming yaml file to match model file

--- lib/main.rb
+++ lib/main.rb
@@ -7,6 +7,6 @@ require_relative "./post_message_definition"
 require_relative "./template/base"
 require_relative "./template/typescript"
 
-post_message_definitions = PostMessageDefinition.load_file("./lib/definitions.yml")
+post_message_definitions = PostMessageDefinition.load_file("./lib/post_message_definition.yml")
 
 Template::Typescript.save("packages/typescript/src/generated.ts", post_message_definitions: post_message_definitions)
